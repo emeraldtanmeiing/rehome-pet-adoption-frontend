addbc97ba794073efc534027da4f3425daad8b40
emeraldtanmeiing <emeraldtanmeiing@gmail.com>
2022-04-07 17:12:54 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      helo 
+    </div>
+  );
+}
+
+export default App;
