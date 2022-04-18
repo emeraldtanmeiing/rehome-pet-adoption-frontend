d9a8f1a87cabba07775d8d56129076b2cbd81165
emeraldtanmeiing <emeraldtanmeiing@gmail.com>
2022-04-18 18:30:17 +0800

    remove unused import

--- src/pages/pets-listing.js
+++ src/pages/pets-listing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {fetchPets} from "../services/pet.service";
+// import {fetchPets} from "../services/pet.service";
 
 function PetsListing() {
   const pets = fetchPets();
